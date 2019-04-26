6691313f9e4dc7fbf7ffa5990d33d74b317b0890
ryan6921 <rmkaw@asu.edu>
2019-04-25 21:35:43 -0700

    Additional changes to teleport.js

--- teleport.js
+++ teleport.js
@@ -6,7 +6,27 @@ function displayResults(teleportTester) {
        $('#results-list').append(
         `<li><h3><a target="_blank" href="">${Object.values(teleportTester.categories[test])}</a></h3>
         </li>`
-      )};
+      )}};
+    
+    
+      function testTeleport(){
+        const searchURL = `https://api.teleport.org/api/urban_areas/slug:san-francisco-bay-area/scores`;
+        console.log(searchURL);
+         fetch(searchURL)
+           .then(response => {
+             if (response.ok) {
+               return response.json();
+             }
+             throw new Error(response.statusText);
+           })
+           .then(responseJson => displayResults(responseJson))
+           .catch(err => {
+             $('#js-error-message').text(`Something went wrong: ${err.message}`);
+             $('#results').addClass('hidden');
+           });
+       }
+
+testTeleport();
 
 
       // displays an unordered list of the categories and the score associated with them
