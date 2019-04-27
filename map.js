f9904161cc60bb9a7559644856d5ddccd419734c
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-04-26 20:36:15 -0700

    started maps.js

---
+++ map.js
@@ -0,0 +1,47 @@
+'use strict'
+
+// map.flyTo({center: e.features[0].geometry.coordinates});
+// map.flyTo({center: coordinates});
+
+// function renderCityMap(){
+// return $('.jsMapReturn').join(
+// `<div id='map' style='width: 400px; height: 300px;'></div>
+// <script>
+// mapboxgl.accessToken = 'pk.eyJ1IjoiamVhbmluZWgiLCJhIjoiY2p1dW9pOWdhMGw0bTQzcWhnOTgyYXVraiJ9.ol6UTML3-IKNBMqdncW2Mw';
+// var map = new mapboxgl.Map({
+// container: 'map',
+// style: 'mapbox://styles/mapbox/streets-v11'
+// });
+// </script>`) 
+// }
+
+function getMap(long, lat) {
+    const params = {
+      cityLocation: long, lat,
+      api_key: 'sk.eyJ1IjoiamVhbmluZWgiLCJhIjoiY2p1eXhvc2Y3MTJ2MzQzcHAzOGdybHcxbyJ9.Y5pWdiwa9_I4yGgPnKYAmA'
+    };
+    
+  
+    fetch(url)
+      .then(response, function (event) {
+        if (response.ok) {
+          let responseJson = response.json();
+          return responseJson;
+      }
+      });
+        
+        throw new Error(response.statusText);
+    
+      .then(responseJson, function (event){
+        displayMap(responseJson)
+      }); 
+        .catch(err, function (event)  {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      });
+  }
+  
+
+endpoint: https://api.mapbox.com/v4/mapbox.satellite.json?access_token=pk.eyJ1IjoiamVhbmluZWgiLCJhIjoiY2p1dW9pOWdhMGw0bTQzcWhnOTgyYXVraiJ9.ol6UTML3-IKNBMqdncW2Mw
+
+let token = 'sk.eyJ1IjoiamVhbmluZWgiLCJhIjoiY2p1eXhvc2Y3MTJ2MzQzcHAzOGdybHcxbyJ9.Y5pWdiwa9_I4yGgPnKYAmA';
+
