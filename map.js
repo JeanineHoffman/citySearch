ccea5b35c045157d94fd7081b41cf8e1dd7261bf
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-04-27 11:45:17 -0700

    first draft map code

--- map.js
+++ map.js
@@ -15,26 +15,38 @@
 // </script>`) 
 // }
 
+function getCityLatLong()
+const textName = $('.citySt').val();
+const apiKey='sk.eyJ1IjoiamVhbmluZWgiLCJhIjoiY2p1eXhvc2Y3MTJ2MzQzcHAzOGdybHcxbyJ9.Y5pWdiwa9_I4yGgPnKYAmA';
+
+fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/$(textName).json?access_token=pk.eyJ1IjoiamVhbmluZWgiLCJhIjoiY2p1dW9pOWdhMGw0bTQzcWhnOTgyYXVraiJ9.ol6UTML3-IKNBMqdncW2Mw')
+  .then(response, function(event){
+    if (response.ok){
+      let latLong = response.Json();
+    }
+     return latLong;
+  })
+    .catch(err, function(event)  {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    })
+
 function getMap(long, lat) {
     const params = {
       cityLocation: long, lat,
       api_key: 'sk.eyJ1IjoiamVhbmluZWgiLCJhIjoiY2p1eXhvc2Y3MTJ2MzQzcHAzOGdybHcxbyJ9.Y5pWdiwa9_I4yGgPnKYAmA'
     };
-    
-  
     fetch(url)
       .then(response, function (event) {
         if (response.ok) {
           let responseJson = response.json();
           return responseJson;
       }
-      });
-        
-        throw new Error(response.statusText);
-    
+      throw new Error(response.statusText);
+      })
+
       .then(responseJson, function (event){
         displayMap(responseJson)
-      }); 
+      }) 
         .catch(err, function (event)  {
         $('#js-error-message').text(`Something went wrong: ${err.message}`);
       });
