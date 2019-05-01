f13278a7e48285abdfb138fbcfc0d15ff51470ee
ryan6921 <rmkaw@asu.edu>
2019-04-30 17:25:13 -0700

    testing git issues

---
+++ autocompletetester/map.js
@@ -0,0 +1,35 @@
+'use strict'
+
+// map.flyTo({center: e.features[0].geometry.coordinates});
+// map.flyTo({center: coordinates});
+const loadMap = (cityCoordinates) => {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiamVhbmluZWgiLCJhIjoiY2p1dW9pOWdhMGw0bTQzcWhnOTgyYXVraiJ9.ol6UTML3-IKNBMqdncW2Mw';
+  var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/outdoors-v11', // stylesheet location
+    center: cityCoordinates, // starting position [lng, lat]
+    zoom: 9 // starting zoom
+  });
+  $('.hidden').toggleClass('hidden');
+}
+
+
+function getCityMap(cityName) {
+  const searchURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+  const accessToken = 'pk.eyJ1IjoiamVhbmluZWgiLCJhIjoiY2p1dW9pOWdhMGw0bTQzcWhnOTgyYXVraiJ9.ol6UTML3-IKNBMqdncW2Mw';
+  const queryString = `${encodeURIComponent(cityName)}.json?`;
+  const url = searchURL + queryString + `access_token=${accessToken}`;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        let responseJson = response.json();
+        return responseJson;
+      }
+      throw new Error(response.statusText);
+    })
+    .then(cityGeoCodeInfo => {
+      let coords = cityGeoCodeInfo.features[0].center;
+      loadMap(coords);
+    });
+}
---
+++ autocompletetester/teleport.js
@@ -0,0 +1,63 @@
+'use strict';
+
+function displayResults(teleportTester) {
+  let resultsHTML = teleportTester.categories.map((item, index) => {
+    let barWidth = `${item.score_out_of_10 * 10}%`;
+    let score = Math.round(item.score_out_of_10 * 10) / 10;
+    return `<div id="item-${index}">
+      <div class="category-title"><span>${item.name}</span><span>${score}</span></div>
+      <div class="graph-cont">
+        <div class="bar-graph" style="width:${barWidth};"></div>
+      </div>
+    </div>`
+  }).join('');
+  $('#results-list').append(resultsHTML);
+};
+
+function displaySalaries(teleportTester) {
+  let resultsHTML = teleportTester.salaries.map((item, index) => {
+    let barWidth = item.percentile_50;
+    //console.log(barWidth);
+    let salary = item.percentile_50;
+    return `<div id="item-${index}">
+    <div class="category-title"><span>${item.job.id}</span><span>${salary}</span></div>
+    <div class="graph-cont">
+      <div class="bar-graph" style="width:${barWidth};"></div>
+    </div>
+  </div>`
+  }).join('');
+  //console.log(resultsHTML);
+  $('#results-list').append(resultsHTML);
+};
+
+function getCityStats(placeName) {
+  let teleportBaseURL = 'https://api.teleport.org/api/urban_areas/slug:';
+  const searchURLs = [`${teleportBaseURL}${placeName}/scores`, `${teleportBaseURL}${placeName}/salaries`];
+  Promise.all(searchURLs.map(url =>
+    fetch(url)
+      .then(checkResults)
+      .then(response => response.json())
+      //.then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      })))
+    //$('#results').addClass('hidden');console.log(responseJson);
+    // calls all teleport functions simultaneously
+    .then(data => {
+      const qualityOfLife = data[0];
+      const qualityOfSalaries = data[1];
+      //console.log(data[0], data[1])
+      displayResults(qualityOfLife);
+      // displaySalaries(qualityOfSalaries);
+    })
+}
+
+function checkResults(response) {
+  if (response.ok) {
+    //console.log(response);
+    return Promise.resolve(response);
+  }
+  throw new Error(response.statusText);
+}
+
+//getAllTeleportData();
